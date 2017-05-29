3235f1d66ef5c0c56e5d6d255a0180dba3eaa580
Tim Graupmann <tgraupmann@gmail.com>
2017-05-29 11:26:40 -0700

    Added yellow led

--- XamarinFormsIoT/HelloWorldPage.xaml.cs
+++ XamarinFormsIoT/HelloWorldPage.xaml.cs
@@ -8,71 +8,91 @@ namespace XamarinFormsIoT
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HelloWorldPage : ContentPage
     {
+        /// <summary>
+        /// The LED pin numbers
+        /// </summary>
         private const int LED_PIN_BLUE = 2;
-
         private const int LED_PIN_RED = 10;
+        private const int LED_PIN_YELLOW = 19;
+
+        /// <summary>
+        /// GPIO controller interface for the portable library
+        /// </summary>
+        private Portable_IGPIOController _mGpioController = null;
+
+        /// <summary>
+        /// GPIO Pin interface for the portable library
+        /// </summary>
+        private Portable_IGpioPin _mPinBlue = null;
+        private Portable_IGpioPin _mPinRed = null;
+        private Portable_IGpioPin _mPinYellow = null;
 
         public HelloWorldPage()
         {
             InitializeComponent();
         }
 
-        private Portable_IGPIOController _mGpioController = null;
-
-        private Portable_IGpioPin _mPinBlue = null;
-        private Portable_IGpioPin _mPinRed = null;
-
         protected override void OnAppearing()
         {
-            /*
-            _mStackLayout.Children.Add(new Label { Text = "Scanning..." });
-
-            try
-            {
-                // discover some devices
-                CrossBleAdapter.Current.Scan().Subscribe(scanResult =>
-                {
-                    _mStackLayout.Children.Add(new Label { Text = scanResult.ToString() });
-
-                });
-            }
-            catch (Exception e)
-            {
-                _mStackLayout.Children.Add(new Label { Text = e.Message });
-            }
-            */
-
             base.OnAppearing();
 
+            // handle stopping a sound from playing
             CrossMediaManager.Current.MediaFinished += Current_MediaFinished;
 
+            // get the gpio controller interface
             _mGpioController = DependencyService.Get<Portable_IGPIOController>().GetDefault();
+
+            // validity check
             if (null != _mGpioController)
             {
+                // setup blue led
                 _mPinBlue = _mGpioController.OpenPin(LED_PIN_BLUE);
                 if (null != _mPinBlue)
                 {
                     _mPinBlue.SetDriveMode(Portable_GpioPinDriveMode.Output);
                 }
 
+                // setup red led
                 _mPinRed = _mGpioController.OpenPin(LED_PIN_RED);
                 if (null != _mPinRed)
                 {
                     _mPinRed.SetDriveMode(Portable_GpioPinDriveMode.Output);
                 }
+
+                // setup yellow led
+                _mPinYellow = _mGpioController.OpenPin(LED_PIN_YELLOW);
+                if (null != _mPinYellow)
+                {
+                    _mPinYellow.SetDriveMode(Portable_GpioPinDriveMode.Output);
+                }
             }
         }
 
+        /// <summary>
+        /// Stop playing when media finishes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void Current_MediaFinished(object sender, Plugin.MediaManager.Abstractions.EventArguments.MediaFinishedEventArgs e)
         {
             await CrossMediaManager.Current.Stop();
         }
 
+        /// <summary>
+        /// Click event to play a sound
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void Button_Clicked(object sender, EventArgs e)
         {
             await CrossMediaManager.Current.Play("ms-appx:///Assets/Test.wav");
         }
 
+        /// <summary>
+        /// Turn on the blue led
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnClickBlueOn(object sender, EventArgs e)
         {
             if (null != _mPinBlue)
@@ -82,6 +102,11 @@ namespace XamarinFormsIoT
             }
         }
 
+        /// <summary>
+        /// Turn off the blue led
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnClickBlueOff(object sender, EventArgs e)
         {
             if (null != _mPinBlue)
@@ -91,6 +116,11 @@ namespace XamarinFormsIoT
             }
         }
 
+        /// <summary>
+        /// Turn on the red led
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnClickRedOn(object sender, EventArgs e)
         {
             if (null != _mPinRed)
@@ -100,6 +130,11 @@ namespace XamarinFormsIoT
             }
         }
 
+        /// <summary>
+        /// Turn off the red led
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnClickRedOff(object sender, EventArgs e)
         {
             if (null != _mPinRed)
@@ -109,10 +144,41 @@ namespace XamarinFormsIoT
             }
         }
 
+        /// <summary>
+        /// Turn on the yellow led
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnClickYellowOn(object sender, EventArgs e)
+        {
+            if (null != _mPinYellow)
+            {
+                var pinValue = Portable_GpioPinValue.Low;
+                _mPinYellow.Write(pinValue);
+            }
+        }
+
+        /// <summary>
+        /// Turn off the yellow led
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnClickYellowOff(object sender, EventArgs e)
+        {
+            if (null != _mPinYellow)
+            {
+                var pinValue = Portable_GpioPinValue.High;
+                _mPinYellow.Write(pinValue);
+            }
+        }
+
+        /// <summary>
+        /// Use a portable interface to quit the app
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnClickQuit(object sender, EventArgs e)
         {
-            OnClickBlueOff(null, null);
-            OnClickRedOff(null, null);
             DependencyService.Get<Portable_IQuit>().Quit();
         }
     }
